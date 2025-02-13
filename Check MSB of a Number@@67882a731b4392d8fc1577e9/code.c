b5061f9aba43c9fa166113095470cb7ad31243fd
parasj1234 <paras1473.becse24@chitkara.edu.in>
2025-02-13 11:59:47 +0530

    Solution to Check MSB of a Number

---
+++ Check MSB of a Number@@67882a731b4392d8fc1577e9/code.c
@@ -0,0 +1,16 @@
+// Your code here...
+#include <stdio.h>
+
+
+int main() {
+    int a;
+    scanf("%d",&a);
+    if((1<<31)&a==1){
+        printf("Set");
+    }
+else{
+    printf("Not Set");
+
+}
+return 0;
+}
